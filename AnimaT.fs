f93ae6caa7e3edef8ceb93e94f6ef68955acd185
0918nobita <nobita.0918@gmail.com>
2019-03-03 11:50:55 +0900

    :tada: Initial commit

---
+++ AnimaT.fs
@@ -0,0 +1,11 @@
+﻿module AnimaT
+
+open FParsec.CharParsers
+
+let parserResult = run pfloat "1.2"
+
+match parserResult with
+| Success(result, _, _) ->
+    printfn "Result: %f" result
+| Failure(_) ->
+    ()
---
+++ README.md
@@ -0,0 +1,13 @@
+# AnimaT
+
+**Initlal Conceptual Stage**
+
+Domain Specific Language for Functional Web Animation
+
+inspired by Adobe After Effects
+
+## Development
+
+```
+dotnet run
+```
