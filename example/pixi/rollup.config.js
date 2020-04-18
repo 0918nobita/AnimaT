8336794d685478f6ddc8a40f653e86b4dfcf8869
0918nobita <nobita.0918@gmail.com>
2020-04-18 14:40:03 +0900

    :sparkles: Pixi.js を用いたサンプルを追加

---
+++ example/pixi/rollup.config.js
@@ -0,0 +1,35 @@
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import serve from "rollup-plugin-serve";
+import { terser } from "rollup-plugin-terser";
+
+const production = !process.env.ROLLUP_WATCH;
+
+export default {
+  input: "./main.js",
+  output: {
+    format: "cjs",
+    dir: "dist",
+    sourcemap: true,
+  },
+  plugins: [
+    commonjs(),
+    resolve({
+      browser: true,
+      preferBuiltins: false,
+    }),
+    production && terser(),
+    !production &&
+      serve({
+        open: true,
+        openPage: "/",
+        verbose: true,
+        contentBase: "dist",
+        host: "localhost",
+        port: 1234,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+        },
+      }),
+  ],
+};
